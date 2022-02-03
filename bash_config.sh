1f8750954c2b03bbcc3d0c098e51a83129477c5d
Franco Mendez Z <f@francomendez.com>
2022-02-03 19:45:30 -0300

    feat: Update editor to open files

--- bash_config.sh
+++ bash_config.sh
@@ -1,7 +1,10 @@
 folder=~/Bash
 
-alias obash="atom ~/Bash/"
+alias obash="code ~/Bash/"
 alias sbp="source ~/.bash_profile"
+
+# Google Drive Go package
+alias drive="~/go/bin/drive"
 
 # Ruby
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
@@ -11,7 +14,11 @@ eval $(thefuck --alias)
 
 # Autojump
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
-ja() { j $@ ; atom . ; }
+ja() { j $@ ; code . ; }
+
+# NVM
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
 
 # Android SDK
 export ANDROID_HOME=$HOME/Library/Android/sdk
