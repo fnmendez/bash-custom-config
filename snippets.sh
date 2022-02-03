854c3c670945da89f545aaf4d4b44901b5e6c905
Franco Mendez Z <f@francomendez.com>
2022-02-03 19:44:02 -0300

    feat: Update snippets

--- snippets.sh
+++ snippets.sh
@@ -10,3 +10,18 @@ alias cloudfront='echo "cf_ci #1 #2 —> aws cloudfront create-invalidation --di
 
 alias cf_ci=aws_cloudfront_create_invalidation
 alias s3_push=aws_s3_push
+
+alias osnippets='j snippets; code snippets.css snippets.js'
+
+alias docker_exec="echo 'docker exec -it <id_contenedor> /bin/bash'"
+
+alias zm_exec="docker-compose exec api bundle exec"
+
+
+alias list_node_modules="find . -name "node_modules" -type d -prune | xargs du -chs"
+alias delete_node_modules="find . -name "node_modules" -type d -prune -exec rm -rf '{}' +"
+
+alias ba=". env/bin/activate"
+alias rs="python3 manage.py runserver"
+
+alias gr="yarn clean && yarn build && yarn start"
